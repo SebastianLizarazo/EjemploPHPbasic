abd28969e92771c118d6f9d70ba0a5c494b8b6f7
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-03-18 12:36:25 -0500

    Perfeccionando y decorando el ejemplo

--- index.php
+++ index.php
@@ -1,31 +1,53 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP basico</title>
+
+    <link href="estilo.css" rel="stylesheet">
+</head>
+ <body>
+  <header>Variables y Arreglos</header>
+  <form action="index.php" method="get">
+      <input type="text" name="name" id="name">
+      <input type="submit" value="Enviar">
+  </form>
+ </body>
+</html>
+<h3>Variable sencilla</h3>
 <?php
 $nombre = "Sebastian";
 var_dump($nombre);
 echo gettype($nombre)."</br>" ;
-
-$nombre = 2;
+?>
+<h3>Varaiable y su tipo de dato</h3>
+<?php
+$nombre = 2.3;
 var_dump($nombre);
 echo gettype($nombre)."</br>" ;
-
-$nombre= 10+"20"."</br>";
+?>
+<h3>Variable con valor concatenado</h3>
+<?php
+$nombre= 10+"20";
 var_dump($nombre);
-echo $nombre;
-
-if ($nombre== "Diego"){
-      echo "Hola Diego";
-}else if ($nombre == "Juan"){
-      echo "Hola Juan";
-}else{
-    echo "Wasaaa  $nombre";
-}
-$nombre=0;
+echo $nombre."</br>";
+echo gettype($nombre);
+?>
+<h3>Conversion de datos</h3>
+<?php
+$nombre=8;
 var_dump((bool)$nombre);
 
-
+?>
+<h3>Conversion Implicita</h3>
+<?php
 $nombre=(bool)"231321";//Conversion implicita
 echo $nombre;
 var_dump($nombre);
+?>
 
+<?php
 $arrayUsuarios= array(
        [
         "Nombre"=> "Diego Ojeda",
@@ -56,6 +78,14 @@ foreach ($arrayUsuarios as $usuario) {
 <?php } ?>
 
 <?php
+
+if ($nombre== "Diego"){
+    echo "Hola Diego";
+}else if ($nombre == "Juan"){
+    echo "Hola Juan";
+}else{
+    echo "Wasaaa  $nombre";
+}
 
 class usuario{
     //boolean, integer, float, string
