5801ae4aadc6a791ac1cae9a478512977f2f1e66
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-03-21 22:46:04 -0500

    Agregacion de ciclos, operador ternario y diferentes tipos de sintaxis para cada estructura condicional.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"">
@@ -10,8 +10,9 @@
  <body>
   <header>Variables y Arreglos</header>
   <form action="index.php" method="get">
-      <input type="text" name="name" id="name">
-      <input type="submit" value="Enviar">
+      <label for="name">Nombre</label>
+          <input type="text" name="name" id="name">
+          <button type="submit">Enviar</button>
   </form>
  </body>
 </html>
@@ -29,61 +30,62 @@ echo gettype($nombre)."</br>" ;
 ?>
 <h3>Variable con valor concatenado</h3>
 <?php
-$nombre= 10+"20";
+$nombre= 10+"20";/*El mas es porque esta sumando esos dos valores mas no concatenandolos*/
 var_dump($nombre);
 echo $nombre."</br>";
 echo gettype($nombre);
 ?>
 <h3>Conversion de datos</h3>
-<?php
-$nombre=8;
-var_dump((bool)$nombre);
+    <?php
+    $nombre=8;
+    var_dump((bool)$nombre);
 
-?>
+    ?>
 <h3>Conversion Implicita</h3>
-<?php
-$nombre=(bool)"231321";//Conversion implicita
-echo $nombre;
-var_dump($nombre);
-?>
+    <?php
+    $nombre=(bool)"231321";//Conversion implicita (duda al ingeniero de como funciona o para que es)
+    echo $nombre;
+    var_dump($nombre);
+    ?>
 <h4>Arreglos y matrices</h4>
-<?php
-$arrayUsuarios= array(
-       [
-        "Nombre"=> "Diego Ojeda",
-        "Edad"=>19,
-        "Ciudad"=>"Sogamoso",
-        "Peso"=>55.8,
-        "Administrador"=> false
-       ],
-       [
-        "Nombre" =>"Carlos Ramirez",
-        "Edad"=>65,
-        "Ciudad"=>"Medellin",
-        "Peso"=>80.8,
-        "Administrador"=> True
-       ]
-);
-var_dump($arrayUsuarios);
-?>
-   <section>
+    <?php
+    $arrayUsuarios= array(
+           [
+            "Nombre"=> "Diego Ojeda",
+            "Edad"=>19,
+            "Ciudad"=>"Sogamoso",
+            "Peso"=>55.8,
+            "Administrador"=> false
+           ],
+           [
+            "Nombre" =>"Carlos Ramirez",
+            "Edad"=>65,
+            "Ciudad"=>"Medellin",
+            "Peso"=>80.8,
+            "Administrador"=> True
+           ]
+    );
+    var_dump($arrayUsuarios);
+    ?>
+<section>
        <article>
-       <?php foreach ($arrayUsuarios as $usuario) {
-                 echo $usuario['Nombre'] . "</br>";
-               } ?>
+           <h2>Los valores del arreglo en la casilla Nombre</h2>
+           <?php foreach ($arrayUsuarios as $usuario) {
+                     echo $usuario['Nombre'] . "</br>";
+                   } ?>
        </article>
-   </section>
+</section>
 
 
 <h1>Usuario dueño</h1>
-<?php if ($nombre == "Sebastian"){ ?>
+<?php if ($nombre === "Sebastian"){ ?>
        <h1>Hola <?php echo $nombre; ?></h1>
 <?php }else{ ?>
        <h1>No hay ningun usuario dueño</h1>
 <?php } ?>
 
 <?php
-/*
+
 if ($nombre=="Diego"){
     echo "Hola Diego";
 }else if ($nombre == "Juan"){
@@ -98,7 +100,7 @@ class usuario{
     // resource y null
     private string $nombre = "Sebastian";
 }
-*/
+
 ?>
 <a href="index2.php">Siguiente pagina</a>
 <footer><?php echo $nombre;?>&copy;</footer>
--- index2.php
+++ index2.php
@@ -1,4 +1,109 @@
-<header><?php echo "Nueva pagina"; ?></header>
- </br>
-<a href="index.php">Pagina Inicial</a>
+<html lang="es">
+
+    <body>
+        <header><?php echo "Nueva pagina"; ?></header>
+                <br>
+            <a href="index.php">Pagina Inicial</br></a>
+    </body>
+</html>
+<?php
+
+$calificacion=15;
+
+/*Forma comun de escribir un if con {}*/
+if($calificacion==19 || $calificacion==20){
+    echo "La calificacion es A";
+}elseif ($calificacion>=16 && $calificacion<=18){
+    echo "La calificacion es B";
+}elseif ($calificacion>=13 && $calificacion<=15){
+    echo "La calificacion es C";
+}elseif ($calificacion>=10 && $calificacion<=12){
+    echo "La calificacion es D";
+}elseif ($calificacion>=1 && $calificacion<=9){
+    echo "La calificacion es E";
+}else{
+    echo "La nota no esta dentro del rango";
+}
+?>
+<br>
+<?php
+
+/*Segunda forma de escribir un if sin {}*/
+if($calificacion==19 || $calificacion==20):
+    echo "La calificacion es A"."</br>";
+elseif ($calificacion>=16 && $calificacion<=18):
+    echo "La calificacion es B"."</br>";
+elseif ($calificacion>=13 && $calificacion<=15):
+    echo "La calificacion es C"."</br>";
+elseif ($calificacion>=10 && $calificacion<=12):
+    echo "La calificacion es D"."</br>";
+elseif ($calificacion>=1 && $calificacion<=9):
+    echo "La calificacion es E"."</br>";
+else:
+    echo "La nota no esta dentro del rango"."</br>";
+endif;
+
+$calificacion=11;
+/*Tercera forma de escribir un if con un OPERADOR TERNARIO*/
+echo "La calificacion es: ".(($calificacion==19 || $calificacion==20)? "A":
+                                (($calificacion>=16 && $calificacion<=18)? "B":
+                                    (($calificacion>=13 && $calificacion<=15)? "C":
+                                        (($calificacion>=10 && $calificacion<=12)? "D":
+                                            (($calificacion>=1 && $calificacion<=9)? "E":
+                                                "La nota no esta dentro del rango")))))."</br>";
+
+/*Funcion empty: determina si una variable esta vacia*/
+$nombre="Carlos";
+var_dump($nombre);
+var_dump(empty($nombre));
+if (!empty($nombre)){
+    echo "Su nombre es: ". $nombre."</br>";
+}else{
+    echo "El nombre esta vacio"."</br>";
+}
+
+/*El simbolo de asignar un valor por defecto si no existe uno (??)*/
+$nombre=null;
+echo "Su nombre es: ".($nombre ?? "Sin nombre")."</br>";
+/*Ciclo while*/
+$n=10;
+while ($n >=1){
+    echo $n."</br>";
+    $n--;
+}
+/*Ciclo do while*/
+$n=10;
+do{
+    echo $n."</br>";
+    $n--;
+}while($n>=1);
+
+/*Ciclo for*/
+for ($i=10;$i>=1;$i--){
+    echo $i."</br>";
+    if ($i==5){
+        break;/*break finaliza la ejecucion de la estructura for,foreach,while,do while o switch en curso*/
+    }
+}
+/*Ciclo foreach*/
+$arrayNombres = [ "Diego","Juan","Pedro","Luis","Carlos" ];
+
+foreach ($arrayNombres as $nombre){
+    if($nombre == "Pedro"):
+        break;
+    endif;
+    echo $nombre."</br>";
+}
+/*Mostrar los datos de un array como un foreach pero con un for*/
+for ($i=0;$i < count($arrayNombres);$i++){
+    echo $arrayNombres[$i]."</br>";
+}
+
+$nombre="20"."10";
+var_dump((bool)$nombre);/*Para ver(duda al ingeniero)*/
+echo $nombre;
+
+
+
+?>
 
